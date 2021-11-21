5261b0814e4595c8ae771d7315eb2a724ef20964
Rakibul Islam Khan <62658122+Rakibul-Islam-Khan@users.noreply.github.com>
2021-11-21 15:26:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const { MongoClient } = require('mongodb')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rirnk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get('/', (req, res) => {
+    res.send('hello i am here')
+})
+app.listen(port)
